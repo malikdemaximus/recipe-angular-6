5d874b6df38fd51226feb00e8889dbc74863a0bf
malikdemaximus <abdulmalik.ermekbai@gmail.com>
2019-01-24 14:33:14 +0600

    Setup First Commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import {ShoppingListService} from './shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
